e58ee1af42671c6bdd1e01d5592061fad74e289f
ASH <ayushsinghd49@gmail.com>
2025-10-06 22:04:07 +0530

    basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Introduction from "./components/Introduction";
+import UpcomingEvents from "./components/UpcomingEvents";
+import EventHighlight from "./components/EventHighlight";
+import BlogNews from "./components/BlogNews";
+import ClickSpark from "./components/ClickSpark";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <ClickSpark sparkColor="#ff0" sparkCount={10} sparkRadius={20}>
+        <Header />
+        <main>
+          <Hero />
+          <Introduction />
+          <UpcomingEvents />
+          <EventHighlight />
+          <BlogNews />
+        </main>
+      </ClickSpark>
+    </div>
+  );
 }
 
-export default App
+export default App;
